ca3086f34ca4bbf4444f0dd96e60e22a1ea35327
TheGreat <mirindano111@gmail.com>
2024-11-03 02:18:05 +0700

    update 03/11/2024

---
+++ src/main/java/vn/hoidanit/laptopshop/Service/UserService.java
@@ -0,0 +1,12 @@
+package vn.hoidanit.laptopshop.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String handleHello() {
+        return "Hello from Service";
+    }
+
+}
